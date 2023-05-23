1582f1dadc2d5cf38d1131a0a2d5d4af13c64a8a
koresuki <kore3019@gmail.com>
2023-05-23 17:08:32 +0800

    add function of youtube-dl

--- Clipboard/src/main/java/com/ke/clipboard/controller/CopyTextController.java
+++ Clipboard/src/main/java/com/ke/clipboard/controller/CopyTextController.java
@@ -5,8 +5,12 @@ import com.ke.clipboard.model.CopyText;
 import com.ke.clipboard.service.CopyTextService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.util.List;
 
@@ -15,6 +19,9 @@ import java.util.List;
 public class CopyTextController {
     @Autowired
     CopyTextService copyTextService;
+
+    @Value("${directory:}")
+    private String directory;
 
     @RequestMapping(value = "/add")
     public  Result<Integer> add(@RequestBody String msg) throws ParseException {
@@ -38,5 +45,68 @@ public class CopyTextController {
     public Result<Void> remark(@RequestParam(value = "id") Integer id){
         copyTextService.remark(id);
         return Result.success();
+    }
+
+    @GetMapping("/formats")
+    public String getVideoFormats(@RequestParam("url") String videoUrl)  {
+        try {
+            String command = String.format("youtube-dl -F %s", videoUrl);
+            Process process = Runtime.getRuntime().exec(command);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            process.waitFor();
+
+            return output.toString();
+        } catch (Exception e) {
+            log.error("",e);
+            return "Error occurred: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/download")
+    public String downloadVideo(@RequestParam("url")  String videoUrl, @RequestParam("formatCode")  String formatCode) throws IOException {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+            String command;
+
+            if (os.contains("win")) {
+                command = String.format("youtube-dl.exe -o \"%s\\%%(title)s.%%(ext)s\" -f %s %s",
+                        directory, formatCode, videoUrl);
+            } else {
+                command = String.format("youtube-dl -o %s/\"%%(title)s.%%(ext)s\" -f %s %s",
+                        directory, formatCode, videoUrl);
+            }
+
+            ProcessBuilder processBuilder;
+            if (os.contains("win")) {
+                processBuilder = new ProcessBuilder("cmd", "/c", command);
+            } else {
+                processBuilder = new ProcessBuilder("bash", "-c", command);
+            }
+
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            process.waitFor();
+
+            return output.toString();
+        } catch (IOException | InterruptedException e) {
+            log.error("",e);
+            return "Error occurred: " + e.getMessage();
+        }
     }
 }
